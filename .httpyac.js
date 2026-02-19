e7c356f4b83c7c1b8ee506e86b2c8ac9214881f7
Mauro Longano <mauro.longano@marconirovereto.it>
2026-02-19 23:04:02 +0100

    feat: add initial .httpyac.js configuration for API testing

---
+++ .httpyac.js
@@ -0,0 +1,13 @@
+module.exports = {
+  defaultHeaders: {
+    'Content-Type': 'application/json'
+  },
+  log: {
+    level: 'warn',
+    supportAnsiColors: true
+  },
+  cookieJarEnabled: false,
+  request: {
+    timeout: 5000
+  }
+};
